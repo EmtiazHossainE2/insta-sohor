423dc6211e22f6a901e8ad5ad1f52c6645010da3
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-10 21:55:12 +0600

    comment and user display them

--- js/app.js
+++ js/app.js
@@ -122,7 +122,7 @@ const createPost = (post) => {
                       <a class="post__name--underline" href="#">
                           ${post.comments[0].user}
                       </a>
-                      ${post.comments.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
